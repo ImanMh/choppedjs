60f422bc58917267b92790d80653ee1d45dcab52
Iman Mohamadi <turbosianor@gmail.com>
2014-07-04 13:27:14 +0430

    test folder removed

--- test/unit/chopped.js
+++
@@ -1,6 +0,0 @@
-
-test('chopper is loaded and ready in the page', function () {
-  var ch = chopped.onScroll(function () {}, 100, 'testChopper');
-  ok(ch !== null, 'chopper instance is not null');
-  ok(ch !== undefined, 'chopper instance is not undefined');
-});
