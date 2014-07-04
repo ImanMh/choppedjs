637fa01bdcb6f833279ed92b087f596c1db1251e
Iman Mohamadi <turbosianor@gmail.com>
2014-07-04 17:46:59 +0430

    initial version of library is read to be used

--- src/chopped.js
+++ src/chopped.js
@@ -1,41 +1,9 @@
 
 (function () {
-  
-  window.chopped = {};
 
-function ChopScroll (handler, timeout, name) {
-  this.timeout = timeout;
-  this.handler = handler;
-  this.name = name || 'unnamed';
-  this.isExecuteTime = true;
-  this.interval = '';
-  var _this = this;
-  init();
+  window.choppedjs = {};
 
-  function init() {
-    //reset the execute determiner
-    $(window).scroll(function () {
-      _this.isExecuteTime = true;
-    });
-
-    //execute the handler based on the timeout user passed
-    _this.interval = setInterval(function () {
-      if (_this.isExecuteTime) {
-        try {
-          handler();
-        } catch (err) {
-          console.log(err);
-        }
-        //turn off the exec time until user scroll again
-        _this.isExecuteTime = false;
-      }
-    }, _this.timeout);
-  }
-
-}
-
-
-  choppedjs.onEvent = function (eventType, handler, timeoutm, options) {
+  choppedjs.onEvent = function (eventType, handler, timeout, options) {
     this.eventType = eventType;
     this.timeout = timeout;
     this.handler = handler;
@@ -43,7 +11,8 @@ function ChopScroll (handler, timeout, name) {
     this.isExecuteTime = true;
     this.interval = '';
     var _this = this;
-    
+    init();
+
     function init() {
       //reset the execute determiner
       $(window)[_this.eventType](function () {
@@ -58,20 +27,20 @@ function ChopScroll (handler, timeout, name) {
           } catch (err) {
             console.log(err);
           }
-         //turn off the exec time until user scroll again
+          //turn off the exec time until user scroll again
           _this.isExecuteTime = false;
         }
       }, _this.timeout);
     }
-    
+
   };
 
   choppedjs.onScroll = function (handler, timeout, options) {
-    choppedjs.onEvent('scroll', handler, timeout, options);
+    return new choppedjs.onEvent('scroll', handler, timeout, options);
   };
 
   choppedjs.onResize = function (handler, timeout, options) {
-    choppedjs.onEvent('resize', handler, timeout, options);
+    return new choppedjs.onEvent('resize', handler, timeout, options);
   };
 
 }());
